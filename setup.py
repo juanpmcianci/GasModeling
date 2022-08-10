cb95aa7010eecc718614e9cb8319e026d1cdaa8b
juanpmcianci <juanpablo@advancedblockchain.com>
2022-08-10 16:34:43 -0500

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 14:34:37 2022
+Set up file for the gas modeling project
+@author: juan
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='GasModeling', version='1.0', packages=find_packages())
